718cfe0cf75f3babb50daec27137f548110975fb
rxlok <xyznuts@gmail.com>
2026-05-31 12:42:20 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 import bcrypt from 'bcryptjs';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 const ROLES = {
